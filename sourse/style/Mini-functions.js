8711e7b6d1f525a328ad7f7c669020ef76bcc41a
Pavel Pavlenko <136743498+areallypawa@users.noreply.github.com>
2024-02-24 21:17:42 +0300

    Ввод в LS
    Сделал наконец функции для работы с элементами

--- sourse/style/Mini-functions.js
+++ sourse/style/Mini-functions.js
@@ -1,11 +1,44 @@
 const toastTrigger = document.getElementById('liveToastBtn')
 const toastLiveExample = document.getElementById('liveToast')
 var header = document.getElementById("header23")
+
+
+
+
+fetch("storage.json")
+.then(function(response){
+    return response.json();
+})
+.then(function(data){
+    localStorage.setItem("products", JSON.stringify(data));
+    if(!localStorage.getItem("cart")){
+        localStorage.setItem("cart", "[]");
+
+    }
+});
+
+let products = JSON.parse(localStorage.getItem("products")); //Все продукты 
+let cart = JSON.parse(localStorage.getItem("cart")); //все товары в корзине
+const cards = document.querySelectorAll(".product__info")
+
+
+let n1 = 0
+let n2 = 0
+let n3 = 0
+let n4 = 0
+let n5 = 0
+let n6 = 0
+let n7 = 0
+let n8 = 0
+
 if (toastTrigger) {
     const toastBootstrap = bootstrap.Toast.getOrCreateInstance(toastLiveExample)
     toastTrigger.addEventListener('click', () => {
         header.innerText = header.textContent = "Пицца 'Сплошной сыр' добавлена в корзину!";
         toastBootstrap.show()
+        addItemToCard(1);
+        n1++;
+        updateQuantity(1, n1);
     })
 }
 
@@ -16,6 +49,9 @@ if (toastTrigger2) {
     toastTrigger2.addEventListener('click', () => {
         header.innerText = header.textContent = "Пицца 'Карбонара' добавлена в корзину!";
         toastBootstrap.show()
+        addItemToCard(2);
+        n2++;
+        updateQuantity(2, n2);
     })
 }
 
@@ -26,6 +62,9 @@ if (toastTrigger3) {
     toastTrigger3.addEventListener('click', () => {
         header.innerText = header.textContent = "Пирог 'Ветчина и сыр'добавлен в корзину!";
         toastBootstrap.show()
+        addItemToCard(3);
+        n3++;
+        updateQuantity(3, n3);
     })
 }
 const toastTrigger4 = document.getElementById('liveToastBtn4')
@@ -35,6 +74,9 @@ if (toastTrigger4) {
     toastTrigger4.addEventListener('click', () => {
         header.innerText = header.textContent = "Пирог 'Чоризо барбекю' добавлен в корзину!";
         toastBootstrap.show()
+        addItemToCard(4);
+        n4++;
+        updateQuantity(4, n4);
     })
 }
 const toastTrigger5 = document.getElementById('liveToastBtn5')
@@ -44,6 +86,9 @@ if (toastTrigger5) {
     toastTrigger5.addEventListener('click', () => {
         header.innerText = header.textContent = "Комбо от Смешариков добавлено в корзину!";
         toastBootstrap.show()
+        addItemToCard(5);
+        n5++;
+        updateQuantity(5, n5);
     })
 }
 const toastTrigger6 = document.getElementById('liveToastBtn6')
@@ -53,6 +98,9 @@ if (toastTrigger6) {
     toastTrigger6.addEventListener('click', () => {
         header.innerText = header.textContent = "Комбо 'Всего по немногу' добавлено в корзину!";
         toastBootstrap.show()
+        addItemToCard(6);
+        n6++;
+        updateQuantity(6, n6);
     })
 }
 const toastTrigger7 = document.getElementById('liveToastBtn7')
@@ -62,6 +110,9 @@ if (toastTrigger7) {
     toastTrigger7.addEventListener('click', () => {
         header.innerText = header.textContent = "Какао добавлено в корзину!";
         toastBootstrap.show()
+        addItemToCard(7);
+        n7++;
+        updateQuantity(7, n7);
     })
 }
 const toastTrigger8 = document.getElementById('liveToastBtn8')
@@ -71,31 +122,15 @@ if (toastTrigger8) {
     toastTrigger8.addEventListener('click', () => {
         header.innerText = header.textContent = "Таежный чай, 10 сашетов добавлен в корзину!";
         toastBootstrap.show()
+        addItemToCard(8);
+        n8++;
+        updateQuantity(8, n8);
     })
 }
 
 
 
 
-const cards = document.querySelectorAll(".product__info")
-
-
-cards.forEach((el, idx) => {
-    console.dir(el)
-    const btn = el.childNodes[7]
-    const title = el.childNodes[1].innerText
-    const price = el.childNodes[3].innerText
-
-    btn.addEventListener("click", () => {
-        const cartStorage = localStorage.getItem("cart") || "[]"
-        const cart = JSON.parse(cartStorage)
-        const card = { title, price }
-        localStorage.setItem("cart", JSON.stringify([...cart, card]))
-    })
-
-})
-
-// console.log(cards)
 
 
 
@@ -104,12 +139,45 @@ cards.forEach((el, idx) => {
 
 
 
+//Функция добавления в корзину
+
+function addItemToCard(productId){
+    let product = products.find(function(product){
+        return product.id == productId;
+    });
+
+    if(cart.lenght == 0){
+        cart.push(product);
+    }else{
+        let res = cart.find(element => element.id == productId);
+        if(res == undefined){
+            cart.push(product);
+        }
+    }   
+    
+    localStorage.setItem("cart", JSON.stringify(cart));
+}
+// addItemToCard(1);
+// addItemToCard(2);
+// addItemToCard(6);
 
 
+function removeItemfromCart(productId){
+    let temp = cart.filter(item => item.id != productId);
+    localStorage.setItem("cart", JSON.stringify(temp));
+    
+}
 
+function updateQuantity(productId, quantity){
+    for(let product of cart){
+        if(product.id == productId){
+            product.quantity = quantity;
+        }
+    }
+    localStorage.setItem("cart", JSON.stringify(cart));
 
+}
 
-
-
+// removeItemfromCart();
 
 
